3dda355505d3cc4fdcbcf9c33add1bde68592873
ise.atsushi <ise.atsushi@raccoon.ne.jp>
2023-11-19 15:26:11 +0900

    別のホストのmysqlを使えるように

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -29,7 +29,8 @@ echo "delete from user_presents where id > 100000000000" | mysql -u"$ISUCON_DB_U
 DIR=`mysql -u"$ISUCON_DB_USER" -p"$ISUCON_DB_PASSWORD" -h "$ISUCON_DB_HOST" -Ns -e "show variables like 'secure_file_priv'" | cut -f2`
 SECURE_DIR=${DIR:-/var/lib/mysql-files/}
 
-sudo cp 5_user_presents_not_receive_data.tsv ${SECURE_DIR}
+scp 5_user_presents_not_receive_data.tsv $ISUCON_DB_HOST:/tmp/
+ssh $ISUCON_DB_HOST "sudo cp /tmp/5_user_presents_not_receive_data.tsv ${SECURE_DIR}"
 
 echo "LOAD DATA INFILE '${SECURE_DIR}5_user_presents_not_receive_data.tsv' REPLACE INTO TABLE user_presents FIELDS ESCAPED BY '|' IGNORE 1 LINES ;" | mysql -u"$ISUCON_DB_USER" \
         -p"$ISUCON_DB_PASSWORD" \
